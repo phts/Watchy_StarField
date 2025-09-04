3316df3b873d85b2d53e861d3542a310752bc22f
Phil Tsaryk <phil@tsaryk.com>
2025-09-04 14:19:31 +0200

    feat(ui): week day with 3 letters

--- WatchyStarField.cpp
+++ WatchyStarField.cpp
@@ -168,7 +168,7 @@ void WatchyStarField::drawDate()
   uint16_t w, h;
 
   String dayOfWeek = dayStr(currentTime.Wday);
-  dayOfWeek = dayOfWeek.substring(0, dayOfWeek.length() - 3);
+  dayOfWeek = dayOfWeek.substring(0, 3);
   display.getTextBounds(dayOfWeek, 5, 85, &x1, &y1, &w, &h);
   if (currentTime.Wday == 4)
   {
